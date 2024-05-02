66f6d7bac0ec3ee5bc7e2d06947b70191373f5ae
MarlonVolff <116438923+MarlonVolff@users.noreply.github.com>
2024-05-02 20:12:14 -0300

    VooUpgrade.cs
    add cod

---
+++ ListaExer10/ListaExer10/VooUpgrade.cs
@@ -0,0 +1,27 @@
+public class VooUpgrade : Voo , IReserva{
+    
+        public VooUpgrade(string origem, string destino, string data) : base(origem, destino, data)
+        {
+        }
+
+        public void reservar()
+        {
+            Console.WriteLine("Reserva na primeira classe efetuada com sucesso!");
+        }
+
+        public void cancelar()
+        {
+            Console.WriteLine("Reserva na primeira classe com sucesso!");
+        }
+
+        public  void status()
+        {
+            Console.WriteLine("Voo primeira classe");
+            Console.WriteLine("origem: {origem}");
+            Console.WriteLine("destino: {destino}, ");
+            Console.WriteLine("data: {data}, ");
+
+
+        }
+
+}
