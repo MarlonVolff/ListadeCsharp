e9c9f5e06002eeaa34cc9dd4f061d75a7d44f843
MarlonVolff <116438923+MarlonVolff@users.noreply.github.com>
2024-05-02 20:13:34 -0300

    Voo.cs
    add cod

---
+++ ListaExer10/ListaExer10/Voo.cs
@@ -0,0 +1,16 @@
+public class Voo : IReserva{
+
+    public string origem {get; set;}
+
+    public string destino {get; set;}
+
+    public string data {get; set;}
+
+    public Voo( string origem, string destino, string data){
+
+        this.origem = origem;
+        this.destino = destino;
+        this.data = data;
+        
+    }
+}
